826e70aba8b3ed3d0a4f6a6f38acbe2ff5492f9f
Ruben <rubenrodriguezcatrufo@gmail.com>
2025-01-21 09:56:29 +0100

    Opciones admin

--- Codigo/app/view/PHP/opcionesAdmin.php
+++ Codigo/app/view/PHP/opcionesAdmin.php
@@ -1,14 +1,19 @@
 <?php
-require_once(__DIR__ . '/../../../rutas.php'); 
-require_once(CONTROLLER . 'ProductoController.php'); 
+require_once(__DIR__ . '/../../../rutas.php');
+require_once(CONTROLLER . 'ProductoController.php');
 require_once(MODEL . 'Producto.php');
 
 session_start();
+if (isset($_SESSION['nombre_usuario'])) {
+    $nombre_usuario = $_SESSION['nombre_usuario'];
+} else {
+    $nombre_usuario = null;
+}
 
 // solo se puede acceder a las opciones de admin si el usuario logeado es admin
-    // nombre_usuario: admin
-    // contraseña: admin
-    
+// nombre_usuario: admin
+// contraseña: admin
+
 if (!isset($_SESSION['nombre_usuario']) || $_SESSION['nombre_usuario'] !== 'admin') {
     echo "<h3>Acceso denegado</h3>";
     echo "<p>No tienes permisos de admin.</p>";
@@ -16,52 +21,83 @@ if (!isset($_SESSION['nombre_usuario']) || $_SESSION['nombre_usuario'] !== 'admi
     exit();
 }
 ?>
-<h1>Opciones Admin</h1>
 
-<h3>Crear producto</h3>
+<!DOCTYPE html>
+<html lang="en">
 
-<!--Formato de la ruta desde /NosaSports/Codigo/app/view/Img/imagenEjemplo.jpg -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../CSS/opcionesAdmin.css">
+</head>
 
-<form action="opcionesAdmin.php" method="POST">
-    <input type="hidden" name="formCreate" value="crearProducto">
+<body>
+    <?php include "../Generales/nav.php"; ?>
+    <h1>Opciones Admin</h1>
 
-    Nombre: <input type="text" name="nombre_producto" required><br>
-    Precio: <input type="number" step="0.01" name="precio" required><br>
-    Descripción: <textarea name="descripcion" required></textarea><br>
-    Deporte: 
-    <select id="deporte" name="deporte" required>
-        <option value="fútbol">Fútbol</option>
-        <option value="tenis">Tenis</option>
-        <option value="baloncesto">Baloncesto</option>
-        <option value="boxeo">Boxeo</option>
-    </select><br>
-    Likes: <input type="number" name="likes" required><br>
-    Imagen:<input type="text" name="imagen" required><br>
-    <input type="submit" value="Crear Producto">
-</form>
+    <!--Formato de la ruta desde /NosaSports/Codigo/app/view/Img/imagenEjemplo.jpg -->
 
-<br>
-<h3>Actualizar producto</h3>
-<form action="opcionesAdmin.php" method="POST">
-    <input type="hidden" name="formUpdate" value="updateProducto">
+    <div>
+        <form action="opcionesAdmin.php" method="POST">
+            <h3>Crear producto</h3>
+            <input type="hidden" name="formCreate" value="crearProducto">
 
-    ID: <input type="number" name="id_producto" required><br>
-    Nombre: <input type="text" name="nombre_producto" required><br>
-    Precio: <input type="number" step="0.01" name="precio" required><br> <!-- step=0.01 solo admite números máximo 2 decimales-->
-    Descripción: <textarea name="descripcion" required></textarea><br>
-    Deporte: <input type="text" name="deporte" required><br>
-    Likes: <input type="number" name="likes" required><br>
-    Imagen:<input type="text" name="imagen" required><br>
-    <input type="submit" value="Actualizar producto">
-</form>
+            <b>Nombre:</b><br>
+            <input type="text" name="nombre_producto" required><br>
 
-<br>
-<h3>Eliminar producto</h3>
-<form action="opcionesAdmin.php" method="POST">
-    <input type="hidden" name="formDelete" value="eliminarProducto">
-    ID: <input type="number" name="id_producto" required><br>
-    <input type="submit" value="Eliminar producto">
-</form>
+            <b>Precio:</b><br>
+            <input type="number" step="0.01" name="precio" required><br>
+
+            <b>Descripcion:</b><br>
+            <textarea name="descripcion" required></textarea><br>
+
+            <b>Deporte:</b><br>
+            <select id="deporte" name="deporte" required>
+                <option value="fútbol">Fútbol</option>
+                <option value="tenis">Tenis</option>
+                <option value="baloncesto">Baloncesto</option>
+                <option value="boxeo">Boxeo</option>
+            </select><br>
+            <b>Likes:</b><br>
+            <input type="number" name="likes" required><br>
+
+            <b>Imagen:</b><br>
+            <input type="text" name="imagen" required><br>
+            <input type="submit" value="Crear Producto" style="background-color: rgb(104,86,52); color: white">
+        </form>
+
+        <br>
+
+        <form action="opcionesAdmin.php" method="POST">
+            <h3>Actualizar producto</h3>
+            <input type="hidden" name="formUpdate" value="updateProducto">
+
+            <b>ID:</b><br> <input type="number" name="id_producto" required><br>
+            <b>Nombre:</b><br> <input type="text" name="nombre_producto" required><br>
+            <b>Precio:</b><br> <input type="number" step="0.01" name="precio" required><br> <!-- step=0.01 solo admite números máximo 2 decimales-->
+            <b>Descripcion:</b><br> <textarea name="descripcion" required></textarea><br>
+            <b>Deporte:</b><br> <input type="text" name="deporte" required><br>
+            <b>Likes:</b><br> <input type="number" name="likes" required><br>
+            <b>Imagen:</b><br><input type="text" name="imagen" required><br>
+            <input type="submit" value="Actualizar producto" style="background-color: rgb(104,86,52); color: white">
+        </form>
+
+        <br>
+
+        <form action="opcionesAdmin.php" method="POST">
+            <h3>Eliminar producto</h3>
+            <input type="hidden" name="formDelete" value="eliminarProducto">
+            <b>ID:</b><br>
+            <input type="number" name="id_producto" required><br>
+            <input type="submit" value="Eliminar producto" style="background-color: red;  color: white">
+        </form>
+    </div>
+
+    <?php  include "../Generales/footer.php" ?>
+</body>
+
+</html>
 
 <?php
 
@@ -80,7 +116,7 @@ if (isset($_POST['formCreate']) && $_POST['formCreate'] == 'crearProducto') {
 
         // validar precio
         if (filter_var($precio, FILTER_VALIDATE_FLOAT)) {
-            $productController->crearProducto($nombreProducto, $precio, $descripcion, $deporte, $likes,$imagen);
+            $productController->crearProducto($nombreProducto, $precio, $descripcion, $deporte, $likes, $imagen);
             echo "<p>Se ha creado el producto " . $nombreProducto . ".</p>";
         } else {
             echo "<p>Precio no válido.</p>";
@@ -104,7 +140,7 @@ if (isset($_POST['formUpdate']) && $_POST['formUpdate'] == 'updateProducto') {
 
         // validar precio
         if (filter_var($precio, FILTER_VALIDATE_FLOAT)) {
-            $productController->modificarProducto($idProducto, $nombreProducto, $precio, $descripcion, $deporte, $likes,$imagen);
+            $productController->modificarProducto($idProducto, $nombreProducto, $precio, $descripcion, $deporte, $likes, $imagen);
             echo "<p>Se ha modificado el producto " . $nombreProducto . ".</p>";
         } else {
             echo "<p>Precio no válido.</p>";
